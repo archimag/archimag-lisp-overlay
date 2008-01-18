9cb50b2a4aace326a374af8e02ac55140e4d215e
Stelian Ionescu <sionescu@common-lisp.net>
2008-01-18 22:25:21 +0100

    app-emacs/slime: load some contribs by default
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- app-emacs/slime/files/70slime-gentoo.el
+++ app-emacs/slime/files/70slime-gentoo.el
@@ -1,10 +1,21 @@
 ;;; site-lisp configuration for slime
 
+(require 'cl)
+
 (add-to-list 'load-path "@SITELISP@")
 (add-to-list 'load-path "@SITELISP@/contrib")
 (autoload 'slime-highlight-edits-mode "slime-highlight-edits")
+
 (require 'slime)
-(slime-setup)
+
+(destructuring-bind (day month year &rest rest)
+    (cdddr (parse-time-string slime-protocol-version))
+  (if (> year 2007)
+      ;; Loading a default set of contribs for post-split SLIME:
+      ;; pretty much what got loaded by default before the split
+      (slime-setup '(slime-fancy slime-asdf slime-banner))
+    ;; Pre-split SLIME
+    (slime-setup)))
 
 ;; this prevents us from requiring the user get dev-lisp/hyperspec
 ;; (which is non-free) as a hard dependency
