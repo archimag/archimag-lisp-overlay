f6f1774b7d85eb83b69f898145643b205438baa2
Stelian Ionescu <sionescu@common-lisp.net>
2008-02-02 21:53:51 +0100

    dev-lisp/flexichain: bump to version 0.3
    Make loading ASDF a little less verbose for lisps that don't bundle it.
    
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- dev-lisp/gentoo-init/files/gentoo-init.lisp
+++ dev-lisp/gentoo-init/files/gentoo-init.lisp
@@ -1,7 +1,12 @@
 (in-package #:cl-user)
 
 #+(or sbcl ecl allegro) (require :asdf)
-#-(or sbcl ecl allegro) (load #p"/usr/share/common-lisp/source/asdf/asdf.lisp")
+#-(or sbcl ecl allegro)
+(let ((*compile-print* nil)
+      (*compile-verbose* nil)
+      #+cmu (ext:*gc-verbose* nil))
+  (handler-bind ((style-warning #'muffle-warning))
+    (load #p "/usr/share/common-lisp/source/asdf/asdf.lisp")))
 
 (push #p"/usr/share/common-lisp/systems/" asdf:*central-registry*)
 
