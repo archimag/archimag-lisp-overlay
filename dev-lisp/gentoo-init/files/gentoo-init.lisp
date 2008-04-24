148f0707eed165bb3549b4804fba4fcfac0f5981
Stelian Ionescu <sionescu@common-lisp.net>
2008-04-24 15:20:57 +0200

    dev-lisp/gentoo-init: fix small typo
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- dev-lisp/gentoo-init/files/gentoo-init.lisp
+++ dev-lisp/gentoo-init/files/gentoo-init.lisp
@@ -6,7 +6,7 @@
       (*compile-verbose* nil)
       #+cmu (ext:*gc-verbose* nil))
   (handler-bind ((style-warning #'muffle-warning))
-    (load #p "/usr/share/common-lisp/source/asdf/asdf.lisp")))
+    (load #p"/usr/share/common-lisp/source/asdf/asdf.lisp")))
 
 (push #p"/usr/share/common-lisp/systems/" asdf:*central-registry*)
 
