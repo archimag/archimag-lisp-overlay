c12de192aa1c93c9eed06d031f7ae285c5ca2475
Stelian Ionescu <sionescu@common-lisp.net>
2008-08-01 01:52:37 +0200

    app-emacs/slime: style changes to 70slime-gentoo.el
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- app-emacs/slime/files/9999/70slime-gentoo.el
+++ app-emacs/slime/files/9999/70slime-gentoo.el
@@ -1,16 +1,15 @@
-;;; site-lisp configuration for slime
+;;; site-lisp configuration for SLIME
 
 (add-to-list 'load-path "@SITELISP@")
 (add-to-list 'load-path "@SITELISP@/contrib")
 (autoload 'slime-highlight-edits-mode "slime-highlight-edits")
 
 (require 'slime)
-
 (slime-setup '(slime-fancy slime-asdf slime-banner))
 
 ;; this allows us not to require dev-lisp/hyperspec
 ;; (which is non-free) as a hard dependency
-
-(if (file-exists-p "/usr/share/doc/hyperspec/HyperSpec")
-    (setq common-lisp-hyperspec-root "file:///usr/share/doc/hyperspec/HyperSpec/")
-  (setq common-lisp-hyperspec-root "http://www.lispworks.com/reference/HyperSpec/"))
+(setq common-lisp-hyperspec-root
+      (if (file-exists-p "/usr/share/doc/hyperspec/HyperSpec")
+          "file:///usr/share/doc/hyperspec/HyperSpec/"
+        "http://www.lispworks.com/reference/HyperSpec/"))
