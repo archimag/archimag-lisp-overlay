0b70b057b68dd2152f9e3e69cc1be034fed57cd7
Stelian Ionescu <sionescu@common-lisp.net>
2009-01-05 00:49:47 +0100

    app-emacs/slime-9999: Update 70slime.el, add new patch to fix a typo

--- app-emacs/slime/files/9999/70slime-gentoo.el
+++ app-emacs/slime/files/9999/70slime-gentoo.el
@@ -6,7 +6,7 @@
 
 (require 'slime-autoloads)
 (eval-after-load 'slime
-  (slime-setup '(slime-fancy slime-asdf slime-banner)))
+  (slime-setup '(slime-fancy slime-repl slime-asdf slime-banner)))
 
 ;; this allows us not to require dev-lisp/hyperspec
 ;; (which is non-free) as a hard dependency
