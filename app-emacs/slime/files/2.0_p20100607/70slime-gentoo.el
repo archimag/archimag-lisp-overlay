802f6d98d181f90997ff171dc1331cfcc4a0cfd0
Moskvitin Andrey <archimag@gmail.com>
2010-06-08 13:10:03 +0400

    app-emacs/slime - bump to version 2.0_p20100512

--- app-emacs/slime/files/2.0_p20100607/70slime-gentoo.el
+++ app-emacs/slime/files/2.0_p20100607/70slime-gentoo.el
@@ -6,8 +6,7 @@
 
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy slime-repl slime-asdf slime-banner
-               slime-compiler-notes-tree slime-enclosing-context
-               slime-sprof))
+               slime-compiler-notes-tree slime-sprof))
 
 ;; this allows us not to require dev-lisp/hyperspec
 ;; (which is non-free) as a hard dependency
