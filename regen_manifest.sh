36e10247b68364dfd4813dfa1f0ef7ad28dca6c7
Andrew N Golovkov <andrew.golovkov@gmail.com>
2010-09-20 21:52:21 +0400

    dev-lisp/terminfo: mask; dev-lisp/linedit: mask;

--- regen_manifest.sh
+++ regen_manifest.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [[ "$*" ]]; then
+	find ./* -name Manifest -delete
+fi
+
 find ./* -iname '*.ebuild' | \
 { [[ $@ ]] && grep $@ || cat; } | \
-xargs -P2 -I{} ebuild {} digest --force >/dev/null
+xargs -P2 -I{} ebuild {} digest --force
