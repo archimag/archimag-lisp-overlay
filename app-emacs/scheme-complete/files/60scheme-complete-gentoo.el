69fe150b4b840decf1661b85b87f57940ef21118
Ulrich Mueller <ulm@gentoo.org>
2010-10-07 20:06:22 +0200

    scheme-complete was moved to portage tree.

--- app-emacs/scheme-complete/files/60scheme-complete-gentoo.el
+++
@@ -1,9 +0,0 @@
-
-;;; scheme-complete site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'scheme-smart-complete "scheme-complete" nil t)
-(autoload 'scheme-complete-or-indent "scheme-complete" nil t)
-(autoload 'scheme-get-current-symbol-info "scheme-complete")
-(eval-after-load 'scheme
-  '(progn (define-key scheme-mode-map "\t" 'scheme-complete-or-indent)))
